6972c1ea68dd058781ed153264805473bcf9800c
RoLegs <rolegs@etik.com>
2022-05-30 12:56:15 +0200

    DronesBasicLab

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,13 @@
 // Iteration #1
+const { model, Schema } = require('mongoose')
+
+const droneSchema = new Schema({
+
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+
+})
+
+const DroneModel = model('drone', droneSchema)
+module.exports = DroneModel
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,41 @@
 // Iteration #1
+const drones = [{
+        name: 'Creeper XL 500',
+        propellers: 3,
+        maxSpeed: 12
+    },
+    {
+        name: 'Racer 57',
+        propellers: 4,
+        maxSpeed: 20
+    },
+    {
+        name: 'Courier 3000i',
+        propellers: 6,
+        maxSpeed: 18
+    },
+]
+
+
+const mongoose = require("mongoose")
+
+require('../db')
+
+const DroneModel = require('../models/Drone.model')
+
+DroneModel.insertMany(drones)
+    .then((allDronesDB) => {
+        console.log('All drones are seeded(added) in your DB', allDronesDB)
+            // return mongoose.connection.close()
+    })
+    // .then(() => {
+    //     console.log('Your DB was closed')
+    // })
+    .catch((error) => {
+        console.log('There is a problem with your seeding', error)
+
+    })
+    .finally(() => {
+        console.log('Everything is okay and was closed')
+        mongoose.connection.close()
+    })
