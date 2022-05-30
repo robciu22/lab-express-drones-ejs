b400562962162738863e434e64dc712794b56f3b
RoLegs <rolegs@etik.com>
2022-05-30 21:42:31 +0200

    DronesBasicLab

--- routes/drones.js
+++ routes/drones.js
@@ -26,34 +26,37 @@ router.get('/drones/create', async(req, res, next) => {
 })
 
 router.post('/drones/create-form', async(req, res, next) => {
-    const newDrone = await DroneModel.create(req.body)
-    res.redirect('/drones')
-    console.log('DRone was created')
-        // const { name, propellers, maxSpeed } = req.body
-        // drone.create({ name, propellers, maxSpeed })
-        //     .then(() => res.redirect(`/drones`))
-        //     .catch(error => next(error))
+    try {
+        // const newDrone = await DroneModel.create(req.body)
+        res.redirect('/drones')
+        console.log('Drone was created')
+    } catch (error) {
+        console.log('Error occured while creating drone')
+    }
 })
 
 
+// Iteration #4: Update the drone
+// ... your code here
 router.get('/drones/:id/edit', async(req, res, next) => {
-    // Iteration #4: Update the drone
-    // ... your code here
-    const drone = await DroneModel.findById(req.params.droneId)
+    // const drone = await DroneModel.findById(req.params.droneId)
     res.render('drones/update-form')
 });
 
 router.post('/drones/:id/edit', async(req, res, next) => {
-    // Iteration #4: Update the drone
-    // ... your code here
-    const { droneId } = req.params
-    await DroneModel.findByIdAndUpdate(droneId, req.body)
-    res.redirect(`/drones/${movieId}`)
+    try {
+        const { droneId } = req.params
+        await DroneModel.findByIdAndUpdate(droneId, req.body)
+        res.redirect('/drones')
+    } catch (error) {
+        console.log('Error occured while updating drone')
+    }
 });
 
+
+// Iteration #5: Delete the drone
+// ... your code here
 router.post('/drones/:id/delete', async(req, res, next) => {
-    // Iteration #5: Delete the drone
-    // ... your code here
     await DroneModel.findByIdAndDelete(req.params.droneId)
     res.redirect('/drones')
 });
